fa3a6d1b9aa9a96d1dd6eab5d782f62a181f3c2b
Codercxf <18251716983@163.com>
2021-06-29 00:40:32 +0800

    first commit

--- src/impl/syncclient.cpp
+++ src/impl/syncclient.cpp
@@ -1,28 +1,44 @@
-#include "syncclient.h"
+/*
+ * Copyright (C) Alex Nekipelov (alex@nekipelov.net)
+ * License: MIT
+ */
 
-namespace redisclient{
-// 构造函数
+#ifndef REDISCLIENT_REDISSYNCCLIENT_CPP
+#define REDISCLIENT_REDISSYNCCLIENT_CPP
+
+#include <memory>
+#include <functional>
+
+#include "redisclient/redissyncclient.h"
+#include "redisclient/pipeline.h"
+#include "redisclient/impl/throwerror.h"
+
+namespace redisclient {
+
 RedisSyncClient::RedisSyncClient(boost::asio::io_service &ioService)
-    :pimpl(std::make_shared<RedisClientImpl>(ioService)),
+    : pimpl(std::make_shared<RedisClientImpl>(ioService)),
     connectTimeout(boost::posix_time::hours(365 * 24)),   // 连接超时时间
     commandTimeout(boost::posix_time::hours(365 * 24)),   // 命令超时时间
-    tcpNoDelay(true), tcpKeepAlive(false)                 // 设置Nagle算法和TCP层的长连接
+    tcpNoDelay(true), 
+    tcpKeepAlive(false)                 // 设置Nagle算法和TCP层的长连接
 {
-
+    pimpl->errorHandler = std::bind(&RedisClientImpl::defaulErrorHandler, std::placeholders::_1);
 }
 
-// 移动构造
-RedisSyncClient::RedisSyncClient(RedisSyncClient &&other) 
+RedisSyncClient::RedisSyncClient(RedisSyncClient &&other)   // 移动构造（右值，使用std::move）
     : pimpl(std::move(other.pimpl)),
     connectTimeout(std::move(other.connectTimeout)),
     commandTimeout(std::move(other.commandTimeout)),
     tcpNoDelay(std::move(other.tcpNoDelay)),
-    tcpKeepAlive(std::move(other.tcpKeepAlive)) 
+    tcpKeepAlive(std::move(other.tcpKeepAlive))
 {
-
 }
-RedisSyncClient::~RedisSyncClient() {
-    pimpl.close();
+
+
+RedisSyncClient::~RedisSyncClient()
+{
+    if (pimpl)
+        pimpl->close();
 }
 
 void RedisSyncClient::connect(const boost::asio::ip::tcp::endpoint &endpoint)
@@ -52,7 +68,7 @@ void RedisSyncClient::connect(const boost::asio::ip::tcp::endpoint &endpoint,
     addr.sin_addr.s_addr = inet_addr(endpoint.address().to_string().c_str());
 
     // Set non-blocking
-    // 标准的设置非阻塞模式
+    // 标准的设置非阻塞模式步骤
     int arg = 0;
     if ((arg = fcntl(socket, F_GETFL, NULL)) < 0)
     {
@@ -70,12 +86,13 @@ void RedisSyncClient::connect(const boost::asio::ip::tcp::endpoint &endpoint,
         return;
     }
 
-    // connecting
-    // Unix的connect函数
+    
     int result = ::connect(socket, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
-    // 阻塞监听套接字，接受新的连接
+    // 因为我们使用的是非阻塞模式，所以如果是返回-1的话并不代表连接失败
     if (result < 0)
     {
+        // 当我们以非阻塞的方式来进行连接的时候，返回的结果如果是 -1,这并不代表这次连接发生了错误，如果它的返回结果是 EINPROGRESS，
+        // 那么就代表连接还在进行中。 后面可以通过poll或者select来判断socket是否可写，如果可以写，说明连接完成了。
         if (errno == EINPROGRESS)
         {
             for(;;)  // 非阻塞模式，需要死循环进行监听
@@ -85,8 +102,9 @@ void RedisSyncClient::connect(const boost::asio::ip::tcp::endpoint &endpoint,
                 pfd.fd = socket;
                 pfd.events = POLLOUT;
 
+                // 判断是否可写
                 result = ::poll(&pfd, 1, connectTimeout.total_milliseconds());
-
+                // 如果也不可写的话
                 if (result < 0)
                 {
                     if (errno == EINTR)
@@ -101,6 +119,7 @@ void RedisSyncClient::connect(const boost::asio::ip::tcp::endpoint &endpoint,
                         return;
                     }
                 }
+                // 如果可写的话，说明是连接成功了
                 else if (result > 0)
                 {
                     // check for error
@@ -141,6 +160,7 @@ void RedisSyncClient::connect(const boost::asio::ip::tcp::endpoint &endpoint,
             return;
         }
     }
+    // 如果连接成功的话
     // 设置socket的非阻塞模式
     if ((arg = fcntl(socket, F_GETFL, NULL)) < 0)
     {
@@ -157,9 +177,165 @@ void RedisSyncClient::connect(const boost::asio::ip::tcp::endpoint &endpoint,
                 boost::asio::error::get_system_category());
     }
 
+    // 状态设置为connected状态
     if (!ec)
         pimpl->state = State::Connected;
 }
 
+#ifdef BOOST_ASIO_HAS_LOCAL_SOCKETS
+
+void RedisSyncClient::connect(const boost::asio::local::stream_protocol::endpoint &endpoint)
+{
+    boost::system::error_code ec;
+
+    connect(endpoint, ec);
+    detail::throwIfError(ec);
+}
+
+void RedisSyncClient::connect(const boost::asio::local::stream_protocol::endpoint &endpoint,
+        boost::system::error_code &ec)
+{
+    pimpl->socket.open(endpoint.protocol(), ec);
+
+    if (!ec)
+        pimpl->socket.connect(endpoint, ec);
+
+    if (!ec)
+        pimpl->state = State::Connected;
+}
+
+#endif
+
+bool RedisSyncClient::isConnected() const
+{
+    return pimpl->getState() == State::Connected ||
+            pimpl->getState() == State::Subscribed;
+}
+// 断开连接，释放套接字
+void RedisSyncClient::disconnect()
+{
+    pimpl->close();
+}
+// 加入错误处理函数
+void RedisSyncClient::installErrorHandler(
+        std::function<void(const std::string &)> handler)
+{
+    pimpl->errorHandler = std::move(handler);
+}
+// 发送命令，间接调用下面的重载函数
+RedisValue RedisSyncClient::command(std::string cmd, std::deque<RedisBuffer> args)
+{
+    boost::system::error_code ec;
+    RedisValue result = command(std::move(cmd), std::move(args), ec);
+
+    detail::throwIfError(ec);
+    return result;
+}
+// 发送命令，格式类似于 result = redis.command("SET", {"key", "value"});
+// 命令为cmd, args是一个队列保存的{"key", "value"}
+// 间接的调用doSyncCommand
+RedisValue RedisSyncClient::command(std::string cmd, std::deque<RedisBuffer> args,
+            boost::system::error_code &ec)
+{
+    // 如果状态有效的话
+    if(stateValid())
+    {
+        // 把命令放在队列的头部一起一起发送出去
+        args.push_front(std::move(cmd));
+        // 内部调用Impl::socketWrite一起发送至redis server
+        // 并且要设置一个命令的超时时长
+        return pimpl->doSyncCommand(args, commandTimeout, ec);
+    }
+    
+    // 如果是无效状态的话，直接调用RedisValue这个构造函数，返回一个空值NULLtag
+    else
+    {
+        return RedisValue();
+    }
+}
+
+// pipeline发送
+Pipeline RedisSyncClient::pipelined()
+{
+    Pipeline pipe(*this);
+    return pipe;
+}
+
+RedisValue RedisSyncClient::pipelined(std::deque<std::deque<RedisBuffer>> commands)
+{
+    boost::system::error_code ec;
+    RedisValue result = pipelined(std::move(commands), ec);
+
+    detail::throwIfError(ec);
+    return result;
+}
+
+RedisValue RedisSyncClient::pipelined(std::deque<std::deque<RedisBuffer>> commands,
+        boost::system::error_code &ec)
+{
+    if(stateValid())
+    {
+        return pimpl->doSyncCommand(commands, commandTimeout, ec);
+    }
+    else
+    {
+        return RedisValue();
+    }
+}
+// 获取状态
+RedisSyncClient::State RedisSyncClient::state() const
+{
+    return pimpl->getState();
+}
+// 设置状态有效 bool 类型
+bool RedisSyncClient::stateValid() const
+{
+    assert( state() == State::Connected );
+
+    if( state() != State::Connected )
+    {
+        std::stringstream ss;
+
+        ss << "RedisClient::command called with invalid state "
+           << to_string(state());
+
+        pimpl->errorHandler(ss.str());
+        return false;
+    }
+
+    return true;
+}
+
+// 设置连接超时时间
+RedisSyncClient &RedisSyncClient::setConnectTimeout(
+        const boost::posix_time::time_duration &timeout)
+{
+    connectTimeout = timeout;
+    return *this;
+}
+
+// 设置命令超时时间
+RedisSyncClient &RedisSyncClient::setCommandTimeout(
+        const boost::posix_time::time_duration &timeout)
+{
+    commandTimeout = timeout;
+    return *this;
+}
+
+// 设置是否启用Nagle算法
+RedisSyncClient &RedisSyncClient::setTcpNoDelay(bool enable)
+{
+    tcpNoDelay = enable;
+    return *this;
+}
+
+// 设置长短连接
+RedisSyncClient &RedisSyncClient::setTcpKeepAlive(bool enable)
+{
+    tcpKeepAlive = enable;
+    return *this;
+}
 
 }
+
+#endif // REDISCLIENT_REDISSYNCCLIENT_CPP
--- src/syncclient.h
+++ src/syncclient.h
@@ -1,57 +1,96 @@
-#ifndef REDIS_SYNC_CLIENT_H
-#define REDIS_SYNC_CLIENT_H
+/*
+ * Copyright (C) Alex Nekipelov (alex@nekipelov.net)
+ * License: MIT
+ */
+
+#ifndef REDISSYNCCLIENT_REDISCLIENT_H
+#define REDISSYNCCLIENT_REDISCLIENT_H
 
 #include <boost/asio/io_service.hpp>
 #include <boost/noncopyable.hpp>
-#include <boost/array.hpp>
-#include <boost/noncopyable.hpp>
-#include <boost/asio/generic/stream_protocol.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/local/stream_protocol.hpp>
-#include <boost/asio/strand.hpp>
-#include <boost/asio/io_service.hpp>
 
 #include <string>
-#include <string>
+#include <list>
+#include <functional>
+
+#include "redisclient/impl/redisclientimpl.h"
+#include "redisbuffer.h"
 #include "redisvalue.h"
-#include "clientimpl.h"
+#include "config.h"
 
+namespace redisclient {
 
-namespace redisclient{
+class RedisClientImpl;
+class Pipeline;
 
-class RedisSyncClient : boost::noncopyable{  // 不许拷贝
+class RedisSyncClient : boost::noncopyable {
 public:
-    //typedef RedisClientImpl::State State;
+    typedef RedisClientImpl::State State;
 
-    RedisSyncClient(boost::asio::io_service &ioService);  // 创建一个客户端，包括客户端的信息
-    RedisSyncClient(RedisSyncClient &&other);
-    ~RedisSyncClient();
+    REDIS_CLIENT_DECL RedisSyncClient(boost::asio::io_service &ioService);  // 创建一个客户端，包括客户端的信息
+    REDIS_CLIENT_DECL RedisSyncClient(RedisSyncClient &&other);
+    REDIS_CLIENT_DECL ~RedisSyncClient();
 
     // Connect to redis server
-    void connect(
-            const boost::asio::ip::tcp::endpoint &endpoint,                // 连接endpoint
+    REDIS_CLIENT_DECL void connect(
+            const boost::asio::ip::tcp::endpoint &endpoint,                // 连接endpoint,带有error_code
             boost::system::error_code &ec);
 
     // Connect to redis server
-    void connect(
+    REDIS_CLIENT_DECL void connect(
             const boost::asio::ip::tcp::endpoint &endpoint);
 
 #ifdef BOOST_ASIO_HAS_LOCAL_SOCKETS
-    void connect(
+    REDIS_CLIENT_DECL void connect(
             const boost::asio::local::stream_protocol::endpoint &endpoint,
             boost::system::error_code &ec);
 
-    void connect(
+    REDIS_CLIENT_DECL void connect(
             const boost::asio::local::stream_protocol::endpoint &endpoint);
 #endif
+
     // Return true if is connected to redis.
-    inline bool isConnected() const;
+    REDIS_CLIENT_DECL bool isConnected() const;
 
     // disconnect from redis
-    void disconnect();
-    
+    REDIS_CLIENT_DECL void disconnect();
+
+    // Set custom error handler.
+    REDIS_CLIENT_DECL void installErrorHandler(
+        std::function<void(const std::string &)> handler);
+
+    // Execute command on Redis server with the list of arguments.
+    REDIS_CLIENT_DECL RedisValue command(
+            std::string cmd, std::deque<RedisBuffer> args);
+
+    // Execute command on Redis server with the list of arguments.
+    REDIS_CLIENT_DECL RedisValue command(
+            std::string cmd, std::deque<RedisBuffer> args,
+            boost::system::error_code &ec);
+
+    // Create pipeline (see Pipeline)
+    REDIS_CLIENT_DECL Pipeline pipelined();
+
+    REDIS_CLIENT_DECL RedisValue pipelined(
+            std::deque<std::deque<RedisBuffer>> commands,
+            boost::system::error_code &ec);
+
+    REDIS_CLIENT_DECL RedisValue pipelined(
+            std::deque<std::deque<RedisBuffer>> commands);
+
+    // Return connection state. See RedisClientImpl::State.
+    REDIS_CLIENT_DECL State state() const;
+
+    REDIS_CLIENT_DECL RedisSyncClient &setConnectTimeout(
+            const boost::posix_time::time_duration &timeout);
+    REDIS_CLIENT_DECL RedisSyncClient &setCommandTimeout(
+            const boost::posix_time::time_duration &timeout);
+
+    REDIS_CLIENT_DECL RedisSyncClient &setTcpNoDelay(bool enable);
+    REDIS_CLIENT_DECL RedisSyncClient &setTcpKeepAlive(bool enable);
+
 protected:
-    inline bool stateValid() const;
+    REDIS_CLIENT_DECL bool stateValid() const;
 
 private:
     std::shared_ptr<RedisClientImpl> pimpl;
@@ -62,4 +101,9 @@ private:
 };
 
 }
+
+#ifdef REDIS_CLIENT_HEADER_ONLY
+#include "redisclient/impl/redissyncclient.cpp"
 #endif
+
+#endif // REDISSYNCCLIENT_REDISCLIENT_H
